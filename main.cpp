1d110c7e1741029dc652982b8a856965f7c9d963
E. Almqvist <h1g.server@gmail.com>
2020-03-05 20:11:00 +0100

    Created a 'engine' object & stuff

--- main.cpp
+++ main.cpp
@@ -3,24 +3,44 @@
 #include <QVulkanWindow>
 #include "qt/vulkanwindow.h"
 
+class Engine {
+    private:
+	unsigned int width = 1600;
+	unsigned int height = 900;
+
+    public:
+	Engine( unsigned int windowWidth, unsigned int windowHeight ) {
+	    width = windowWidth;
+	    height = windowHeight;
+
+	    qInfo() << "Engine Object Created";
+	}
+
+	int exitcode = 0;	
+
+	int InitalizeWindow( int argc, char * argv[]) {
+	    QApplication qtApp(argc, argv); // give it the args
+	    qInfo() << "Initializing Window";
+	    
+	    QVulkanInstance vulkanInstance;
+	    vulkanInstance.setLayers(QByteArrayList() << "VK_LAYER_LUNARG_standard_validation");
+
+	    if (!vulkanInstance.create())
+		qFatal("Failed to create Vulkan instance: %d", vulkanInstance.errorCode());
+	    
+	    VulkanWindow vulkanWindow; 
+	    vulkanWindow.setVulkanInstance( &vulkanInstance );
+
+	    vulkanWindow.resize( width, height );
+	    vulkanWindow.show();
+
+	    exitcode = qtApp.exec();
+	    return exitcode;
+	}
+};
+
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    // MainWindow w;
-    qInfo() << "Game Engine Init";
-    // w.show();
-
-
-    QVulkanInstance inst;
-    inst.setLayers(QByteArrayList() << "VK_LAYER_LUNARG_standard_validation");
-
-
-    if (!inst.create())
-        qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
-
-    VulkanWindow w;
-    w.setVulkanInstance(&inst);
-
-    w.resize(1024, 768);
-    w.show();
-    return a.exec();
+    Engine game(500,500);
+    game.InitalizeWindow( argc, argv ); // initalize the window
+    return game.exitcode;
 }
