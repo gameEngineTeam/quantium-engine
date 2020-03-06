0f9836cef18beb0a0ab8beace284489a01b4ea61
Alve <alve@hotmail.se>
2020-03-06 08:09:30 +0100

    Indentation fix (spaces: 4)

--- main.cpp
+++ main.cpp
@@ -3,52 +3,57 @@
 #include <QVulkanWindow>
 #include "qt/vulkanwindow.h"
 
-class Engine {
-    private:
-	unsigned int width = 1600;
-	unsigned int height = 900;
-	bool isRunning = false;
+class Engine
+{
+private:
+    unsigned int width = 1600;
+    unsigned int height = 900;
+    bool isRunning = false;
 
-    public:
-	int exitcode = 0;
+public:
+    int exitcode = 0;
 
-	Engine( unsigned int windowWidth, unsigned int windowHeight, bool runOnStart ) {
-	    width = windowWidth;
-	    height = windowHeight;
+    Engine(unsigned int windowWidth, unsigned int windowHeight, bool runOnStart)
+    {
+        width = windowWidth;
+        height = windowHeight;
 
-	    isRunning = runOnStart;
+        isRunning = runOnStart;
 
-	    qInfo() << "Engine Instance Created";
-	}
+        qInfo() << "Engine Instance Created";
+    }
 
-	~ Engine() {
-	    qInfo() << "Engine Instance Deinitialization";
-	}	
+    ~Engine()
+    {
+        qInfo() << "Engine Instance Deinitialization";
+    }
 
-	int Initalize( int argc, char * argv[] ) {
-	    QApplication qtApp(argc, argv); 
-	    qInfo() << "Initializing";
+    int Initalize(int argc, char *argv[])
+    {
+        QApplication qtApp(argc, argv);
+        qInfo() << "Initializing";
 
-	    QVulkanInstance vulkanInstance;
-	    vulkanInstance.setLayers(QByteArrayList() << "VK_LAYER_LUNARG_standard_validation");
+        QVulkanInstance vulkanInstance;
+        vulkanInstance.setLayers(QByteArrayList() << "VK_LAYER_LUNARG_standard_validation");
 
-	    if ( !vulkanInstance.create() )
-		qFatal("Failed to create Vulkan instance: %d", vulkanInstance.errorCode());
+        if (!vulkanInstance.create())
+            qFatal("Failed to create Vulkan instance: %d", vulkanInstance.errorCode());
 
-	    VulkanWindow vulkanWindow; 
-	    vulkanWindow.setVulkanInstance( &vulkanInstance );
+        VulkanWindow vulkanWindow;
+        vulkanWindow.setVulkanInstance(&vulkanInstance);
 
-	    vulkanWindow.resize( width, height );
-	    vulkanWindow.show();
+        vulkanWindow.resize(width, height);
+        vulkanWindow.show();
 
-	    exitcode = qtApp.exec();
-	    return exitcode;
-	}
+        exitcode = qtApp.exec();
+        return exitcode;
+    }
 };
 
-int main(int argc, char *argv[]) {
-    Engine game( 640, 480, false ); // Holy Resolution
-    game.Initalize( argc, argv ); // initalize the window
+int main(int argc, char *argv[])
+{
+    Engine game(640, 480, false); // Holy Resolution
+    game.Initalize(argc, argv);   // initalize the window
 
     return game.exitcode;
 }
