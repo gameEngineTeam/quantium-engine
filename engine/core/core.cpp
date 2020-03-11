28450302a32205ded9b7bf3f83ffad80300da7dd
Alve Svarén <alve@hotmail.se>
2020-03-11 18:16:30 +0100

    Not working right now

--- engine/core/core.cpp
+++ engine/core/core.cpp
@@ -1,7 +1,46 @@
 #include "../engine_core.h"
-
+#include <QSize>
 namespace quantum_engine {
-Window::Window(unsigned short width, unsigned short height) {
+
+// QT Vulkan window wrapper
+Window::Window(unsigned short width, unsigned short height, int argc, char* argv[]) {
     this->visible = false;
+    this->setTitle("Quantum Application");
+    this->qtWindow;
+    this->qtApplication(argc, argv);
+}
+
+void Window::setMinimumSize(unsigned short width, unsigned short height) {
+    this->qtWindow.setMinimumSize(QSize(width,height));
+}
+
+void Window::setMaximumSize(unsigned short width, unsigned short height) {
+    this->qtWindow.setMaximumSize(QSize(width,height));
+}
+
+void Window::show() {
+    this->setVisible(true);
+}
+
+void Window::setVisible(bool visible) {
+    this->visible = visible;
+    if (visible) this->qtWindow.show();
+    else this->qtWindow.hide();
+}
+
+void Window::resize(unsigned short width, unsigned short height) {
+    this->qtWindow.resize(width, height);
+}
+
+void Window::hide() {
+    this->setVisible(false);
+}
+
+bool Window::isVisible() {
+    return this->visible;
+}
+
+void Window::setTitle(std::string title) {
+    this->title = title;
 }
 }  // namespace quantum_engine
--- engine/engine_core.h
+++ engine/engine_core.h
@@ -1,14 +1,23 @@
 #include <iostream>
+#include "../qt/vulkanwindow.h"
+#include <QApplication>
 
 namespace quantum_engine {
 
 class Window {
     bool visible;
+    QApplication qtApplication;
+    std::string title;
+    VulkanWindow qtWindow;
 public:
-    Window(unsigned short width, unsigned short height);
+    Window(unsigned short width, unsigned short height, int argc, char* argv[]);
+    void setVisible(bool visible);
     void show();
     void resize(unsigned short width, unsigned short height);
     void setTitle(std::string title);
+    void setMinimumSize(unsigned short width, unsigned short height);
+    void setMaximumSize(unsigned short width, unsigned short height);
+    std::string getTitle();
     void hide();
     bool isVisible();
 };
